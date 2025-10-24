7d9fa10d3acf481099d232be667b98977ce3781f
Grzegorz Jaruszewski <jaruszewskig@gmail.com>
2025-10-24 14:38:33 +0200

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class App {
+}
