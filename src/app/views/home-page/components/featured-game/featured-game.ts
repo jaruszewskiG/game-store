8495063bfe818049f1d4041485c8bdaf5c0283ab
Grzegorz Jaruszewski <jaruszewskig@gmail.com>
2025-10-28 16:18:43 +0100

    Clean up the project from unused components. Fix backdrop issue on desktop

--- src/app/views/home-page/components/featured-game/featured-game.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-featured-game',
-  imports: [],
-  templateUrl: './featured-game.html',
-  styleUrl: './featured-game.scss',
-})
-export class FeaturedGame {}
