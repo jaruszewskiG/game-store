312cf72ee4465775d0359520d0659eaa7df4178e
Grzegorz Jaruszewski <jaruszewskig@gmail.com>
2025-10-29 10:55:00 +0100

    Unify vscode/settings.json formatting with prettier. Add perettier formatting to pre-commit hook. Fix failing unit tests

--- src/app/components/game-list/game-list-item/game-list-item.spec.ts
+++ src/app/components/game-list/game-list-item/game-list-item.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { GameListItem } from './game-list-item';
 
@@ -6,13 +8,23 @@ describe('GameListItem', () => {
   let component: GameListItem;
   let fixture: ComponentFixture<GameListItem>;
 
+  const mockGame = {
+    id: 1,
+    title: 'Test Game',
+    price: 9.99,
+    imageUrl: 'test.jpg',
+    isOwned: false,
+  } as any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [GameListItem],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GameListItem);
     component = fixture.componentInstance;
+    component.game = mockGame;
     fixture.detectChanges();
   });
 
